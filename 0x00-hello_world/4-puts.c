215aa27f69e431012d1d28836b53f4d5d73f29f2
Worlako Dzokoto <worlako1986@hotmail.com>
2022-10-13 21:13:33 -0700

    Update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * print data using puts and 
+ * Return: Always 0 (Success)
+ */
+
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
