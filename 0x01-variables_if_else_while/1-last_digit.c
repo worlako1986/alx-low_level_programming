fe514b3970447df1037d24cd7115f9397692b514
Worlako Dzokoto <worlako1986@hotmail.com>
2022-10-17 13:03:31 -0700

    Added 1-last_digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: success(0)
+*/
+
+
+int main(void)
+{
+
+	int n;
+	int ld = 0;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (abs(n) >= 10)
+	{
+		ld = (abs(n) % 10);
+	} else
+	{
+		ld = n;
+	}
+
+	if (n < 0)
+	{
+		ld = -1 * ld;
+	}
+
+	if (ld < 6 && ld != 0)
+	{
+		printf("last digit of %d is %d and is less than 6 and not 0\n", n, ld);
+	} else if (ld > 5)
+	{
+		printf("last digit of %d is %d and is greater than 5\n", n, ld);
+	} else
+	{
+		printf("last digit of %d is %d and is 0", n, ld);
+	}
+	return (0);
+
+}
