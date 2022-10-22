d85e41652cd5c842f8944aa279ef5f3c1ec69e2c
Worlako Dzokoto <worlako1986@hotmail.com>
2022-10-22 02:39:13 -0700

    Feat: add print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+
+/* The function print out alpabets */
+
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+
+	int length = sizeof(alphabet) / sizeof(alphabet[0]);
+
+	int i = 0;
+
+	while (i < length)
+	{
+		putchar(alphabet[i]);
+
+		i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
