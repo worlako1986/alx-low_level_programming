caa712712053ee469a3f5fa83ded781cf7d7ca50
Worlako Dzokoto <worlako1986@hotmail.com>
2022-10-22 06:52:10 -0700

    Feat: Add 6-print-numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar('0' + (int long)i);
+
+		i++;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
